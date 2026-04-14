622c8725b19894686a121c06f0538daa56dff10d
shih002581 <d11416147@gs.takming.edu.tw>
2026-04-14 16:12:28 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public interface IName
+{
+    string GetName();
+}
+public class Person : IName
+{
+    public string GetName()
+    {
+        return "Person";
+    }   
+}
+
+public class Student : IName
+{
+    public string GetName()
+    {
+        return "Student";
+    }
+}
+public class Teacher : IName
+{
+    public string GetName()
+    {
+        return "Teacher";
+    }
+}
+public class Caller
+{
+    public void CallGetNname(IName name)
+    {
+        Console.WriteLine(name.GetName());
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Class_0414_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caller c = new Caller();
+            c.CallGetNname(new Person());
+            c.CallGetNname(new Student());
+            c.CallGetNname(new Teacher());
+        }
+    }
+}
